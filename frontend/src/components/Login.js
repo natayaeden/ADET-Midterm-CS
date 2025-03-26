7ab23933f4e7b582641b26e32aabc89e5763eb08
Obrey Monter <monterobrey85@gmail.com>
2025-03-26 20:35:32 +0800

    ui dashboard enhancement

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { motion } from "framer-motion";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./LoginStyles.css"; 
+import "../componentStyles/LoginStyles.css"; 
 
 const Login = () => {
 
