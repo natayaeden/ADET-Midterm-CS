5b434e01e93b6c01c2cc2f1faf97134fe4eb372e
Jeamko23590 <nositerajearemyniko21@gmail.com>
2025-04-21 23:51:10 +0800

    Setted the foreign key of the task and project

--- backend/database/migrations/2025_03_27_131546_create_projects_table.php
+++ backend/database/migrations/2025_03_27_131546_create_projects_table.php
@@ -9,6 +9,7 @@ return new class extends Migration {
     {
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->string('name');
             $table->text('description')->nullable();
             $table->string('project_manager');
